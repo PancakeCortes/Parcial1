26298d94486c561af14cd4eacb3d3090adcb65aa
Rosefall <pancakes2425@gmail.com>
2024-05-06 13:27:45 -0600

    Se añadio la funcionalidad de las tablas del 2 al 5

--- practica1Git/src/main/java/com/example/practica1Git/Practica1GitApplication.java
+++ practica1Git/src/main/java/com/example/practica1Git/Practica1GitApplication.java
@@ -3,13 +3,17 @@ package com.example.practica1Git;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Practica1GitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Practica1GitApplication.class, args);
+
+
 		System.out.println("hola");
 		mostrarTabla();
+		tablas25();
 	}
 
 
@@ -21,5 +25,13 @@ public class Practica1GitApplication {
 		return null;
 	}
 	
+	public static String tablas25() {
+
+		for (int index = 0; index <= 10; index++) {
+			
+			System.out.println("2 x "+index+" = "+index*2+"        "+"3 x "+index+" = "+index*3+"        "+"4 x "+index+" = "+index*4+"        "+"5 x "+index+" = "+index*5);
+		}
+		return null;
+	}
 
 }
